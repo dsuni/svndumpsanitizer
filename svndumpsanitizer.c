d2922a19ae65f75344645bcf45f44f0be6efd0f4
Daniel Suni <daniel.suni@gmail.com>
2015-12-05 10:17:15 +0200

    The previous bug fix for delete node handling didn't fix the issue completely. This one should.

--- svndumpsanitizer.c
+++ svndumpsanitizer.c
@@ -1,5 +1,5 @@
 /*
-	svndumpsanitizer version 2.0.0 RC2, released 29 Nov 2015
+	svndumpsanitizer version 2.0.0 RC3, released 5 Dec 2015
 
 	Copyright 2011,2012,2013,2014,2015 Daniel Suni
 
@@ -33,7 +33,7 @@
 #include <string.h>
 #include <time.h>
 
-#define SDS_VERSION "2.0.0 RC2"
+#define SDS_VERSION "2.0.0 RC3"
 #define ADD 0
 #define CHANGE 1
 #define DELETE 2
@@ -499,6 +499,7 @@ int has_redefine_collisions(repotree *root, repotree *current, char *redefined_r
 void restore_delete_node_if_needed(repotree *rt, node *n) {
 	repotree *target = get_subtree(rt, n->path, 1);
 	int i = target->map_len - 1;
+	int j;
 	char *temp;
 	// Find the right pointer...
 	while (i >= 0 && target->map[i] != n) {
@@ -514,9 +515,15 @@ void restore_delete_node_if_needed(repotree *rt, node *n) {
 		if (target->map[i]->action == ADD && target->map[i]->dep_len > 0 && target->map[i]->deps[0]->wanted && target->map[i]->deps[0]->copyfrom) {
 			temp = add_slash_to(target->map[i]->deps[0]->path);
 			if (starts_with(n->path, temp)) {
-				n->wanted = 3;
 				free(temp);
-				return;
+				temp = get_dir_after_copyfrom(target->map[i]->deps[0]->copyfrom, n->path, target->map[i]->deps[0]->path);
+				for (j = 1; j < target->map[i]->dep_len; ++j) {
+					if (target->map[i]->deps[j]->wanted && strcmp(target->map[i]->deps[j]->path, temp) == 0) {
+						free(temp);
+						n->wanted = 3;
+						return;
+					}
+				}
 			}
 			free(temp);
 		}
